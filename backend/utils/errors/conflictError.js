f51880c29b1bdd15132dfc9e01696b567d642568
Tatiana Petrova <tatianatt187@yandex.ru>
2023-08-19 00:07:30 +0300

    ПР15-3

--- backend/utils/errors/conflictError.js
+++ backend/utils/errors/conflictError.js
@@ -1,10 +1,9 @@
 const {
   HTTP_STATUS_CONFLICT,
-  MESSAGE_ERROR_CONFLICT,
 } = require('../constants');
 
 class ConflictError extends Error {
-  constructor(message = MESSAGE_ERROR_CONFLICT) {
+  constructor(message) {
     super(message);
     this.statusCode = HTTP_STATUS_CONFLICT;
   }
--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -8,13 +8,14 @@ module.exports = {
       about: Joi.string().min(2).max(30),
       avatar: Joi.string().pattern(URL_REGEXP),
       email: Joi.string().required().email(),
-      password: Joi.string().required().min(8),
+      password: Joi.string().required(),
     }),
   }),
 
   validateSignIn: celebrate({
     body: Joi.object().keys({
       email: Joi.string().required().email(),
+      password: Joi.string().required(),
     }),
   }),
 
